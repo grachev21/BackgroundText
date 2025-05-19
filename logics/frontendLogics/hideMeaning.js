bf9918a4bd663e7879535de555f0f13ee4aa20f2
grachev21 <grachev613@gmail.com>
2025-05-19 16:50:27 +0400

    hideMeaning

---
+++ logics/frontendLogics/hideMeaning.js
@@ -0,0 +1,23 @@
+const checkbox = document.getElementById("hideMeaning");
+
+const hideMeaning = () => {
+  const handleCheckboxChange = function () {
+    if (this.checked) {
+      document.querySelectorAll(".post-pair > .me").forEach((element) => {
+        element.style.display = "none";
+      });
+    } else {
+      document.querySelectorAll(".post-pair > .me").forEach((element) => {
+        element.style.display = "block";
+      });
+    }
+  };
+
+  if (checkbox) {
+    checkbox.addEventListener("change", handleCheckboxChange);
+
+    handleCheckboxChange.call(checkbox);
+  }
+};
+
+export default hideMeaning;
--- logics/render.js
+++ logics/render.js
@@ -1,7 +1,9 @@
 import inputPost from "./frontendLogics/InputPost.js";
 import loadPost from "./frontendLogics/loadPost.js";
+import hideMeanign from "./frontendLogics/hideMeaning.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   inputPost();
   loadPost();
+  hideMeanign();
 });
